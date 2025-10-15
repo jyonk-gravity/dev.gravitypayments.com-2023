4d19e9437e349e59c0efa5bad3d8cbc06dd6b39b
Justin Yonk <jyonk@Mac.lan>
2025-10-15 15:27:34 -0600

    Added ability to have password protection on downloads page template

--- wp-content/themes/gravitydocs2020/resources/views/page-downloads.blade.php
+++ wp-content/themes/gravitydocs2020/resources/views/page-downloads.blade.php
@@ -3,6 +3,10 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     {{-- @include('partials.page-header') --}}
-    @include('partials.content-page-downloads')
+    @if ( post_password_required() )
+      {!! get_the_password_form() !!}
+    @else
+      @include('partials.content-page-downloads')
+    @endif
   @endwhile
 @endsection
